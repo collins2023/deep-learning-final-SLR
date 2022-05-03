059a6227b2b88cb2bdaf98385f4ef0f6061f09bf
Dylan Hu <dylanhu7@gmail.com>
2022-05-02 21:49:45 -0400

    feat: refactor and saving weights

---
+++ src/slr.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import os
+from preprocess import get_data
+
+# Sign Language Recognition
+
+
+def main():
+    num_classes = 29
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+
+    train_generator, validation_generator = get_data()
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['sparse_categorical_accuracy'])
+
+    STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
+    STEP_SIZE_VALID = validation_generator.n//validation_generator.batch_size
+
+    checkpoint_path = "checkpoints/cp.ckpt"
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+    model.fit(train_generator,
+              steps_per_epoch=STEP_SIZE_TRAIN,
+              epochs=10,
+              validation_data=validation_generator,
+              validation_steps=STEP_SIZE_VALID,
+              callbacks=[cp_callback])
+
+    model.evaluate(validation_generator, steps=STEP_SIZE_VALID)
+
+
+if __name__ == '__main__':
+    main()
