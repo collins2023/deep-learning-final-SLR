f64e4aa8a911a677142e99f766a9ba66856498a2
Dylan Hu <dylanhu7@gmail.com>
2022-05-02 21:12:22 -0400

    fix: preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,26 @@
 from keras.preprocessing.image import ImageDataGenerator
 
+
 def get_data():
-    base = 'data/'
-    prefix = 'asl_alphabet_'
     batch_size = 100
     pic_size = 48
 
-    datagen_train = ImageDataGenerator()
-    datagen_test = ImageDataGenerator()
+    image_flow = ImageDataGenerator(validation_split=0.1)
 
-    train_generator = datagen_train.flow_from_directory(base + prefix + "train",
-                                                        target_size=(pic_size,pic_size),
-                                                        color_mode="grayscale",
-                                                        batch_size=batch_size,
-                                                        class_mode='categorical',
-                                                        shuffle=True)
+    train_generator = image_flow.flow_from_directory("data/asl_alphabet_train/asl_alphabet_train/",
+                                                     target_size=(
+                                                         pic_size, pic_size),
+                                                     color_mode="grayscale",
+                                                     batch_size=batch_size,
+                                                     class_mode='sparse',
+                                                     subset="training")
 
-    validation_generator = datagen_test.flow_from_directory(base + prefix + "test",
-                                                        target_size=(pic_size,pic_size),
-                                                        color_mode="grayscale",
-                                                        batch_size=batch_size,
-                                                        class_mode='categorical',
-                                                        shuffle=False)
+    validation_generator = image_flow.flow_from_directory("data/asl_alphabet_train/asl_alphabet_train/",
+                                                          target_size=(
+                                                              pic_size, pic_size),
+                                                          color_mode="grayscale",
+                                                          batch_size=batch_size,
+                                                          class_mode='sparse',
+                                                          subset="validation")
 
     return train_generator, validation_generator
